8c45b6d8ebb5e7d8b6caf93f7092ebc950605dfa
Miles Frankel <miles.frankel@gmail.com>
2015-01-30 00:43:33 -0500

    transparency seems to be boolean

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
+import           Data.Word                   (Word8)
+import           Text.Printf
 import           XMonad
+import           XMonad.Actions.GridSelect   hiding (spawnSelected)
 import           XMonad.Actions.Plane
 import           XMonad.Hooks.DynamicLog
 import           XMonad.Hooks.ManageDocks
@@ -16,12 +19,11 @@ import           XMonad.Layout.SimplestFloat
 import           XMonad.Layout.ThreeColumns
 import qualified XMonad.StackSet             as W
 import           XMonad.Util.EZConfig
+import           XMonad.Util.Loggers
 import           XMonad.Util.Run
 
-import           Data.Word                   (Word8)
-import           Text.Printf
-import           XMonad.Actions.GridSelect   hiding (spawnSelected)
-import           XMonad.Util.Loggers
+import           XMonad.Hooks.FadeWindows
+
 
 
 term, startupWorkspace :: String
@@ -48,7 +50,7 @@ defaultLayouts =
                               ||| withGaps ( GridRatio (3/2) ) -- 4 wide, 3 tall
                               ||| Full -- fullscreen
                              ))
-        where withGaps = gaps [(U,10),(D,10),(L,10),(R,10)]
+        where withGaps = gaps $ zip [U,D,L,R] $ repeat 10 -- [(U,10),(D,10),(L,10),(R,10)]
 
 defaultlayout = ResizableTall 1 (3/100) (1/2) []
 
@@ -65,54 +67,60 @@ layouts = onWorkspace "1:Term"  defaultlayout $
 
 managementHooks :: [ManageHook]
 managementHooks =
-    [ className =? "Emacs"                --> doF (W.shift $ workspacesC !! 1)
-    , className =? "xfce4-terminal"       --> doF (W.shift $ workspacesC !! 0)
-    , className =? "terminology"          --> doF (W.shift $ workspacesC !! 0)
-    , className =? "Midori"               --> doF (W.shift $ workspacesC !! 2)
-    , className =? "Thunar"               --> doF (W.shift $ workspacesC !! 3)
+    [ className =? "Emacs"                --> doF (W.shift $ workspace 2)
+    , className =? "xfce4-terminal"       --> doF (W.shift $ workspace 1)
+    , className =? "terminology"          --> doF (W.shift $ workspace 1)
+    , className =? "Thunar"               --> doF (W.shift $ workspace 4)
     , className =? "XTerm"                --> doFloat
-    , className =? "openxcom"             --> doF (W.shift $ workspacesC !! 4)
-    , className =? "MPlayer"              --> doF (W.shift $ workspacesC !! 4)
-    , className =? "Google-chrome-stable" --> doF (W.shift $ workspacesC !! 2)
-    , className =? "Netsurf.elf"          --> doF (W.shift $ workspacesC !! 2)
-    , className =? "Firefox"              --> doF (W.shift $ workspacesC !! 2)
-
+    , className =? "openxcom"             --> doF (W.shift $ workspace 5)
+    , className =? "MPlayer"              --> doF (W.shift $ workspace 5)
+    , className =? "Google-chrome-stable" --> doF (W.shift $ workspace 3)
+    , className =? "Netsurf.elf"          --> doF (W.shift $ workspace 3)
+    , className =? "Firefox"              --> doF (W.shift $ workspace 3)
     ]
+  where workspace n = workspacesC !! (n-1)
 
 myLogHook xmproc = dynamicLogWithPP xmobarPP {
-              ppOutput = hPutStrLn xmproc
-            , ppTitle = xmobarColor "#eeeeee" "" . shorten 50
-            , ppCurrent = xmobarColor "#e6744c" ""
-                          . wrap "[" "]"
+              ppOutput  = hPutStrLn xmproc
+            , ppTitle   = xmobarColor "#eeeeee" "" . shorten 50
+            , ppCurrent = xmobarColor "#e6744c" "" . wrap "[" "]"
             , ppUrgent  = xmobarColor "#82a6df" "#000000"
-            , ppOrder  = \(ws:lay:title:_) -> [ws,title]
+            , ppOrder   = \(ws:lay:title:_) -> [ws,title]
             -- ppExtra for stuff
             }
 
+fadeHook :: FadeHook
+fadeHook = composeAll [ className =? "Firefox"  --> opaque
+                      , transparency 0.2
+                      ]
+
 myConfig xmproc = defaultConfig {
                     focusedBorderColor = "#444444"
-                  , normalBorderColor = "#cccccc"
-                  , terminal = term
-                  , borderWidth = 3
-                  , layoutHook = layouts
-                  , workspaces = workspacesC
-                  , modMask = mod4Mask
-                  , handleEventHook = fullscreenEventHook <+> docksEventHook -- added <+>..
-                  , startupHook = theStartupHook
-                  , manageHook = manageHook defaultConfig
-                                 <+> composeAll managementHooks
-                                 <+> manageDocks
-                  , logHook = myLogHook xmproc
+                  , normalBorderColor  = "#cccccc"
+                  , terminal           = term
+                  , borderWidth        = 3
+                  , layoutHook         = layouts
+                  , workspaces         = workspacesC
+                  , modMask            = mod4Mask
+                  , handleEventHook    = fadeWindowsEventHook <+> fullscreenEventHook <+> docksEventHook
+                                         -- added <+>..
+                  , startupHook        = theStartupHook
+                  , manageHook         = manageHook defaultConfig
+                                         <+> composeAll managementHooks
+                                         <+> manageDocks
+                  , logHook            =  fadeWindowsLogHook fadeHook >> myLogHook xmproc
                   }
 
 theStartupHook :: X ()
 theStartupHook = do
 --  setWMName "LG3D" -- can help java GUI programs work
   windows $ W.greedyView startupWorkspace
-  spawnIfNotRunning term ""               -- start terminal
-  spawn "xloadimage -onroot -zoom 60 ~/images/background.jpg"
+  spawnIfNotRunning "xcompmgr" " -c &"      -- trying to add transparency
+  spawnIfNotRunning term ""                 -- start terminal
+  spawn $ "feh --bg-scale " ++ background_img
+    where background_img = "~/images/background.jpg"
 
-spawnIfNotRunning :: MonadIO m => String -> String -> m ()
+spawnIfNotRunning :: String -> String -> X ()   -- MonadIO => m ... m()
 spawnIfNotRunning cmd argStr =
     spawn $ "if [ -z `pgrep " ++ cmd ++ " ` ] ; then " ++ cmd ++ " " ++ argStr ++ " ;fi"
 
@@ -131,15 +139,18 @@ main = do
                                , ("M-x k", kill)
                                , ("M-l", sendMessage Expand)  -- this is default
                                , ("M-k", sendMessage Shrink)
+
+                               , ("M-S-r", spawn "killall xcompmgr; sleep 1; xcompmgr -c &")
+                                 -- restart xcompmgr if stuff freezes
                                ]                           -- M1 is actual alt (xmodmap)
              `removeKeysP` [("M-S-c")]
   where gsconfig = (buildDefaultGSConfig blackColorizer) { gs_cellheight = 40, gs_cellwidth = 75 }
 
 -- ccw from bottom
 blackColorizer :: HasColorizer a => a -> Bool -> X (String, String)
-blackColorizer str active = if active
-                            then return ("white","black") -- background, text
-                            else return ("black", "white")
+blackColorizer str active = return $ if active
+                                     then ("white","black") -- background, text
+                                     else ("black", "white")
 
 spawnSelected :: GSConfig String -> [(String, String)] -> X ()
 spawnSelected conf lst = gridselect conf lst >>= flip whenJust spawn
